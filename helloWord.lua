717837e491f29f3f0e90412e2b318acad6bcf417
yangjunchao888 <30312828+yangjunchao888@users.noreply.github.com>
2021-11-11 11:59:33 +0800

    Create helloWord.lua

---
+++ helloWord.lua
@@ -0,0 +1,35 @@
+People = {name = "", age = 0, weight = 0, high = 0}
+
+function People:new(o,name,age,weight,height)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+  self.name = name;
+  self.age = age;
+  self.weight = weight;
+  self.height = height;
+  return o
+end
+
+function People:introduce ()
+  print(string.format("my name: %s, my age: %d hello world", self.name, self.age))
+end
+
+mePeople = People:new(nil, "yangjc", 27, 166, 70)
+mePeople:introduce()
+
+Student = People:new()
+function Student:new(o, school)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+  self.school = school
+  return o
+end
+
+function Student:introduce()
+  print(string.format("i'm a student in: %s, hello world", self.school))
+end
+
+MeStudent = Student:new(mePeople, "xxx high school")
+MeStudent:introduce()
